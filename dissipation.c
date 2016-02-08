d188d26e475df042324b073ade910bbb6421fb14
N8Stewart <Nate.Stewart12@gmail.com>
2016-02-08 14:28:44 -0500

    Generate the gridBoxes with contact distances and contact temperatures.

--- dissipation.c
+++ dissipation.c
@@ -43,12 +43,12 @@ int main( int argc, char **argv ) {
 	 */
 	int numGridBoxes, numRows, numCols;
 	scanf("%d %d %d", &numGridBoxes, &numRows, &numCols);
-	box *boxes = malloc(sizeof(box) * numGridBoxes);
+	simpleBox *boxes = malloc(sizeof(simpleBox) * numGridBoxes);
 
 	/*
 	 * Grab each data line 1 by 1
 	 */
-	int i, j;
+	int i, j, k;
 	for( i = 0; i < numGridBoxes; i++ ) {
 		// Line 2: id
 		scanf("%d", &boxes[i].id);
@@ -57,44 +57,21 @@ int main( int argc, char **argv ) {
 		scanf("%d %d %d %d", &(dims -> ul_y), &(dims -> ul_x), &(dims -> height), &(dims -> width));
 		dims -> br_x = dims -> ul_x + dims -> width;
 		dims -> br_y = dims -> ul_y + dims -> height;
-        dims -> perimeter = (dims -> width) * 2 + (dims -> height) * 2;
-		// Line 4: top neighbors
+		
+		// Line 4-7: Neighbors
 		neighbors *currNeighbor;
-		currNeighbor = &boxes[i].n_top;
-		scanf("%d", &(currNeighbor -> num));
-		if( (currNeighbor -> num) > 0 ) {
-			(currNeighbor -> ids) = malloc(sizeof(int) * (currNeighbor -> num));
-			for( j = 0; j < (currNeighbor -> num); j++ ) {
-				scanf("%d", (currNeighbor -> ids) + j);
+		for( j = 0; j < 4; j++ ) {
+			currNeighbor = &boxes[i].nei[j];
+			scanf("%d", &(currNeighbor -> num));
+			if( (currNeighbor -> num) > 0 ) {
+				(currNeighbor -> ids) = malloc(sizeof(int) * (currNeighbor -> num));
+				for( k = 0; k < (currNeighbor -> num); k++ ) {
+					scanf("%d", (currNeighbor -> ids) + k);
+				}
 			}
+			
 		}
-		// Line 5: bottom neighbors
-		currNeighbor = &boxes[i].n_bot;
-		scanf("%d", &(currNeighbor -> num));
-		if( (currNeighbor -> num) > 0 ) {
-			(currNeighbor -> ids) = malloc(sizeof(int) * (currNeighbor -> num));
-			for( j = 0; j < (currNeighbor -> num); j++ ) {
-				scanf("%d", (currNeighbor -> ids) + j);
-			}
-		}
-		// Line 6: left neighbors
-		currNeighbor = &boxes[i].n_left;
-		scanf("%d", &(currNeighbor -> num));
-		if( (currNeighbor -> num) > 0 ) {
-			(currNeighbor -> ids) = malloc(sizeof(int) * (currNeighbor -> num));
-			for( j = 0; j < (currNeighbor -> num); j++ ) {
-				scanf("%d", (currNeighbor -> ids) + j);
-			}
-		}
-		// Line 7: right neighbors
-		currNeighbor = &boxes[i].n_right;
-		scanf("%d", &(currNeighbor -> num));
-		if( (currNeighbor -> num) > 0 ) {
-			(currNeighbor -> ids) = malloc(sizeof(int) * (currNeighbor -> num));
-			for( j = 0; j < (currNeighbor -> num); j++ ) {
-				scanf("%d", (currNeighbor -> ids) + j);
-			}
-		}
+		
 		// Line 8: temperature
 		scanf("%lf", &boxes[i].temp);
 	}
@@ -110,23 +87,77 @@ int main( int argc, char **argv ) {
 		return -1;
 	}
 	printf("All lines read correctly\n");
-    
-    /*
-     * Time to do the math.
-     * Compute the AMR Dissipation to convergence
-     */
-    
-    
+   
+	/*
+	 * Compute the contact distances for all boxes
+	 */
+	gridBox *grid = malloc(sizeof(gridBox) * numGridBoxes);
+	for( i = 0; i < numGridBoxes; i++) {
+		grid[i].temp = boxes[i].temp;
+		grid[i].perimeter = (boxes[i].dims.height + boxes[i].dims.width) * 2;
+		
+		// Calculate how many neighbors there are and allocate memory for neighbor temps and CDs	
+		int numNeighbors = boxes[i].nei[0].num + boxes[i].nei[1].num + boxes[i].nei[2].num + boxes[i].nei[3].num;
+		grid[i].neiTemps = malloc(sizeof(double) * numNeighbors);
+		grid[i].neiCD = malloc(sizeof(int) * numNeighbors);
+		// Pointers to alleviate repetitive typing
+		neighbors *currNeighbor;
+		simpleBox *currBox;
+		// Index of the current neighbor to be put in the current gridBox
+		int neiTempIndex = 0;
+		// Loop through all of the neighbors and calculate the contact distances
+		for( j = 0; j < 4; j++ ) {
+			currNeighbor = &boxes[i].nei[j];
+			for( k = 0; k < (currNeighbor -> num); k++, neiTempIndex++) {
+				// Grab the current simple box and the current dimensions of that box
+				currBox = &boxes[(currNeighbor -> ids)[k]];
+				grid[i].neiTemps[neiTempIndex] = currBox -> temp; // Populate the temperature for the gridBox neighbor 
+				// Slightly different algorithm depending on whether the neighbor is left/right or top/bot
+				if( j < 2 ) { // 0,1 is top and bottom
+					grid[i].neiCD[neiTempIndex] = calculateContactDistance(boxes[i].dims.ul_x, boxes[i].dims.br_x, (currBox -> dims).ul_x, (currBox -> dims).br_x);
+				} else { // 2,3 is left and right
+					grid[i].neiCD[neiTempIndex] = calculateContactDistance(boxes[i].dims.ul_y, boxes[i].dims.br_y, (currBox -> dims).ul_y, (currBox -> dims).br_y);
+				}
+			}
+		}
+		
+
+	}
+
     /*
      * Free the memory of all boxes
      */
     for( i = 0; i < numGridBoxes; i++ ) {
-        free(boxes[i].n_top.ids);
-        free(boxes[i].n_bot.ids);
-        free(boxes[i].n_left.ids);
-        free(boxes[i].n_right.ids);
+        for( j = 0; j < 4; j++) {
+			free(boxes[i].nei[j].ids);
+		}
     }
     free(boxes);
     
+	/*
+     * Time to do the math.
+     * Compute the AMR Dissipation to convergence
+     */
+   	
 	return 0;
 }
+
+int calculateContactDistance(int box1_ul, int box1_br, int box2_ul, int box2_br) {
+	if( box1_ul <= box2_ul ) {
+		if( box1_br >= box2_br ) {
+			return box2_br - box2_ul;
+		} else { // box1_br < box2_br
+			return box1_br - box2_ul;
+		}
+	} else { // box1_ul > box2_ul
+		if( box1_br <= box2_br ) {
+			return box1_br - box1_ul;
+		} else { // Box1_br > box2_br
+			return box2_br - box1_ul;
+		}
+	}
+}
+
+
+
+
--- dissipation.h
+++ dissipation.h
@@ -11,17 +11,23 @@
 #define DEFAULT_AFFECT_RATE 0.1
 
 /*
+ * Calculate the contact distance between two boxes.
+ * Box 1 is the reference box. Box 2 is its neighbor.
+ * UL stands for upper left coordinate. BR stands for bottom right coordinate.
+ */
+int calculateContactDistance(int box1_ul, int box1_br, int box2_ul, int box2_br);
+
+/*
  * Struct to store upper left and bottom right coordinates of the box.
- * Also stores the height and width for calculations
+ * Also stores the height and width for calculations.
  */
 typedef struct {
 	int ul_x, ul_y, br_x, br_y;
 	int height, width;
-    int perimeter;
 } dimensions;
 
 /*
- * Store the number of boxes that are neighbors and the ids of those boxes
+ * Store the number of boxes that are neighbors and the ids of those boxes for use when constructing contact matrix.
  */
 typedef struct {
 	int num; // Number of boxes that are neighbors
@@ -29,13 +35,22 @@ typedef struct {
 } neighbors;
 
 /*
- * Simplistic storage container for all of the information regarding a box
+ * Simplistic storage container for all of the information regarding a box which is read in from the data files.
  */
 typedef struct {
 	int id;
 	dimensions dims;
-	neighbors n_top, n_bot, n_left, n_right;
+	neighbors nei[4]; // 0 = top, 1 = bot, 2 = left, 3 = right
 	double temp;
+} simpleBox;
 
-} box;
-
+/*
+ * Storage container for the dissipation calculation. Only need to store temps, perimeter and contact distance.
+ * This is constructed after all data is read from the data file.
+ */ 
+typedef struct {
+	double temp;
+	int perimeter;
+	double *neiTemps; // Neighbor temps
+	int *neiCD; // Neighbor contact distances
+} gridBox;
